9c7d9596dfef59caade914ac9ff8467877b7bf60
HighW0rks <127955603+HighW0rks@users.noreply.github.com>
2025-09-15 23:03:36 +0200

    Opdracht 2

---
+++ jaar 1/periode 1/opdrachten/opdracht2/opdracht2/Maanden.cs
@@ -0,0 +1,18 @@
+namespace opdracht2
+{
+    public enum Maanden
+    {
+        Januari = 1,
+        Februari = 2,
+        Maart = 3,
+        April = 4,
+        Mei = 5,
+        Juni = 6,
+        Juli = 7,
+        Augustus = 8,
+        September = 9,
+        Oktober = 10,
+        November = 11,
+        December = 12
+    }
+}
