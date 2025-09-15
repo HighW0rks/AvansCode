64502cf510a83a4c2c2560d9ad070f83f6e18c85
HighW0rks <127955603+HighW0rks@users.noreply.github.com>
2025-09-15 23:04:17 +0200

    Created Test code

---
+++ jaar 1/periode 1/opdrachten/test code/VerbeterCode/Program.cs
@@ -0,0 +1,21 @@
+﻿decimal saldo = 0.33m;
+decimal transactieBedrag = 49.95m;
+decimal kredietLimiet = -1000.0m;
+
+if (saldo - transactieBedrag >= 0)
+{
+    saldo -= transactieBedrag;
+    Console.WriteLine("verwerk transactie");
+}
+else if (saldo - transactieBedrag > kredietLimiet)
+{
+    saldo -= transactieBedrag;
+    Console.WriteLine("verwerk transactie");
+    Console.WriteLine("Let op: Saldo negatief");
+}
+else
+{
+     Console.WriteLine("Blokkeer transactie");
+}
+
+Console.WriteLine($"Uw saldo is: {saldo}");
